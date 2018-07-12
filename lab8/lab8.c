40d9f44ca4b92b91682cfc30a8c76bdde8ef1fff
jyny <jyny@hackersir>
2018-07-12 10:18:36 -0700

    lab

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
 
 int main(void){
+	char KEY[8];
 	printf("%s\n", "                                   _  __ ");
 	printf("%s\n", "  _ __ _____   _______      _____ | |/ _|");
 	printf("%s\n", " | \'__/ _ \\ \\ / / _ \\ \\ /\\ / / _ \\| | |_ ");
 	printf("%s\n", " | | |  __/\\ V / (_) \\ V  V / (_) | |  _|");
 	printf("%s\n\n", " |_|  \\___| \\_/ \\___/ \\_/\\_/ \\___/|_|_|  ");
+	printf("Looking for keys? find some overflow:");
+	scanf("%s", KEY);
+	
+	if(*(unsigned int *)(KEY+0) == 0x7265766f){
+		if(*(unsigned int *)(KEY+4) == 0x7265766f){
+			printf("wow you got the key\n");
+		}else{
+			printf("you key is like wolf?\n");
+		}
+	}else{
+		printf("your key is no revo :p\n");
+	}
 }
