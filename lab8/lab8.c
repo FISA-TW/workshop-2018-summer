5b2bb3707b90a6859733e6daf9c49a90ca080d33
jyny <jyny@hackersir>
2018-07-14 10:48:07 -0700

    change

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -2,17 +2,20 @@
 
 int main(void){
 	char KEY[8];
+	char nonkey[7]="overme";
+
 	printf("%s\n", "                                   _  __ ");
 	printf("%s\n", "  _ __ _____   _______      _____ | |/ _|");
 	printf("%s\n", " | \'__/ _ \\ \\ / / _ \\ \\ /\\ / / _ \\| | |_ ");
 	printf("%s\n", " | | |  __/\\ V / (_) \\ V  V / (_) | |  _|");
 	printf("%s\n\n", " |_|  \\___| \\_/ \\___/ \\_/\\_/ \\___/|_|_|  ");
+	printf("%s\n", nonkey);
 	printf("Looking for keys? find some overflow:");
-	scanf("%s", KEY);
+	scanf("%s", nonkey);
 	
 	if(*(unsigned int *)(KEY+0) == 0x7265766f){
-		if(*(unsigned int *)(KEY+4) == 0x7265766f){
-			printf("wow you got the key\n");
+		if(*(unsigned int *)(KEY+4) == 0x776f6c66){
+			printf("flag{you_know_overflow!!}\n");
 		}else{
 			printf("you key is like wolf?\n");
 		}
